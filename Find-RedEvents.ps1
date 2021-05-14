e151b93214567772b57a8dbf3228860d2cfd9291
chris <Chris.Brock@adeccogroupnonprod.onmicrosoft.com>
2021-05-14 18:08:48 +0100

    D

--- Find-RedEvents.ps1
+++ Find-RedEvents.ps1
@@ -1,4 +1,4 @@
-Function Find-RedEvent {
+Function Find-RedEvents {
 
 
 
@@ -6,17 +6,18 @@ Function Find-RedEvent {
 .SYNOPSIS
     Event ID Searcher
 .DESCRIPTION
-    Find all occurences of the specified event ID for the selected computer
+    Find all occurences of the specified event ID for the selected computer and export to a csv file
 .EXAMPLE
     PS C:\> Find-RedEvent
     Will search for the default example event 4648 which shows logged on users for the local computer
 
-            Find-RedEvent -Logname 'System' ID='6005' } -ComputerName Server1
-    Will find all results for ID 6005 of the event log starting on Server1
+            Find-RedEvent -Logname 'System' ID='6005' -ComputerName Server1 -FileLocation 'E:\Output\MonthlyReports\
+    Will find all results for ID 6005 of the event log found on Server1 within the last 24 hours and export to a csv in the specified location
 .INPUTS
     ComputerName
     Event ID
     Logname - This is the root of the log for the chosen event ID   
+    FileLocation - This is where the csv will be saved
 .OUTPUTS
     .
 .NOTES
@@ -33,9 +34,14 @@ Function Find-RedEvent {
     [String]$EventID = '4648',
 
     [Parameter(Mandatory=$false,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
-    [String]$Logname = 'Security'
+    [String]$Logname = 'Security',
+
+    [Parameter(Mandatory=$false,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+    [String]$FileLocation = 'C:\Temp\'
     )
 
+    $Date = (Get-Date).ToString("dd-MM-yyyy")
+    $Filename = "Event-" + $EventID + "-Report-" + $Date + ".csv"
 
 Write-Host "Checking for elevated permissions..." -ForegroundColor Yellow
 
@@ -50,14 +56,25 @@ Write-Host "Console running as administrator, permissions check complete and suf
 
 }
 
+Write-Host "Checking for output directory $FileLocation" -ForegroundColor Green
+
+if(Test-Path $FileLocation){
+
+    write-host "The following path - $FileLocation - is accessible, the file will be saved here with the following filename:  $Filename" -foregroundcolor Green
+    
+    }else{
+
+    Write-Warning "File location inaccessible, please provide a valid location and re-run the script "
+    Break
+
+}
+
 Write-Host "Searching for: $EventID on $ComputerName in the $Logname log" -ForegroundColor Yellow
 
 $Events = Get-WinEvent -FilterHashtable @{Logname=$Logname; ID=$EventID } -ComputerName $ComputerName
 
-$Events
+$Events | select Message,Id,Logname | Export-Csv $FileLocation$Filename -NoTypeInformation
 
 }
 
-#example - find the lastloggedon user
-
-(Find-RedEvent | select -First 1).message
+Find-RedEvents
